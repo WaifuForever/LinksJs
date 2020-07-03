740dee429cb66019b834b68faf4fea0c45eb6045
#51978077 <itachiut1r4@gmail.com>
2020-07-03 16:36:53 -0300

    sequelize configured

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express= require('express');
+const db = require('./models');
+
+const authController = require('./controllers/auth');
+
+const app = express();
+
+app.use('/auth', authController);
+
+app.get('/', (req, res) => {
+    return res.json('Api running...')
+});
+
+db.sequelize.sync().then(() => {
+    app.listen(3000, () => {
+        console.log('Listening on port 3001');
+    });
+});
